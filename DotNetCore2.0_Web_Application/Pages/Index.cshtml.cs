5614192d486a25055381e891819726a404431e99
天涯 青蛙 <liuyonghong159632@163.com>
2017-09-25 16:37:11 +0800

    .net core 2.0 normal projects

---
+++ DotNetCore2.0_Web_Application/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetCore2._0_Web_Application.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ DotNetCore2.0_Web_Application_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCore2._0_Web_Application_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
