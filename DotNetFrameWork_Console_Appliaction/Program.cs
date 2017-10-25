a9d8f3bb6df4e8b82191baccec9749879607e354
天涯 青蛙 <liuyonghong159632@163.com>
2017-10-25 15:48:14 +0800

    添加Quartz 作业调度测试项目 采用.netframework4.6.1

---
+++ DotNetFrameWork_Console_Appliaction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFrameWork_Console_Appliaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
