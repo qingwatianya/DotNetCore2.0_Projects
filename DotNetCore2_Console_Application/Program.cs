ba1fea88e3a7cdc529cd28d6858e0f20d15526d4
天涯 青蛙 <liuyonghong159632@163.com>
2017-10-25 15:16:15 +0800

    添加.net core2.0 控制天应用程序

---
+++ DotNetCore2_Console_Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore2_Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
